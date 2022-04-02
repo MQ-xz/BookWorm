a6e743989242803b2493a7a88a0f160553cbdca8
MQ <kmahshooq@gmail.com>
2022-04-02 17:25:04 +0530

    Fix

--- NoteBook/forms.py
+++ NoteBook/forms.py
@@ -6,9 +6,7 @@ from .models import *
 from django.db.models import Q
 
 
-
 class SignUpForm(UserCreationForm):
-
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
